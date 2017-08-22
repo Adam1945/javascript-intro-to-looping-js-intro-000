12b09d1faa6cedc78d5fb730b15b2b0d090a4396
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 19:38:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(array) {
-  array = [];
-  for(var i = 0; i < 25; i++) {
-    
-  }
-    
-}
+// function forLoop(array) {
+//   array = [];
+//   for(var i = 0; i < 25; i++) {
+//
+//   }
+//
+// }
 
 function whileLoop(number) {
   while(number <= 0) {
